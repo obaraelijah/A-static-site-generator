1f6bb0cc46cffeaa255ac3591258469a537441af
elijah obara <elijahobara357@gmail.com>
2023-01-25 23:11:05 +0300

    Added file

--- statGenerator.py
+++ statGenerator.py
@@ -1,3 +1,7 @@
 import os
 import markdown2
 from jinja2 import Environment, FileSystemLoader
+
+def generate_site(input_folder, output_folder):
+    # Create the Jinja2 environment
+    env = Environment(loader=FileSystemLoader('templates'))
